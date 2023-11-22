06cec8a913daf7315c40eeb3f75deecd2af77447
rhara <rhara.ianna@estudante.ufjf.br>
2023-11-21 22:51:09 -0300

    adiciona atualizar lista

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+import {getLista, adicionaNaLista, removeDaLista,limpaLista} from "../src/lista.js"
+
 const nEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
-adicionaElementoNaLista("primeiro");
-adicionaElementoNaLista("segundo");
-adicionaElementoNaLista("terceiro");
+atualizarListaOrdenada();
+
+function atualizarListaOrdenada(){
+    const lista = getLista();
+    for(let i=0; i<lista.length; i++)
+    {
+        adicionaElementoNaLista(lista[i]);
+    }
+}
+
 
 function adicionaElementoNaLista(texto){
     const li = document.createElement("li");
