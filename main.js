2000b1bfb815f34ea763c44047df1bd83bd9b501
rhara <rhara.ianna@estudante.ufjf.br>
2023-11-21 22:59:29 -0300

    adiciona item no estado

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 import {getLista, adicionaNaLista, removeDaLista,limpaLista} from "../src/lista.js"
 
-const nEntrada = document.querySelector("#entrada");
+const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
 atualizarListaOrdenada();
+btnAdicionar.addEventListener('click',adicionaItemDaEntrada);
+
 
 function atualizarListaOrdenada(){
     const lista = getLista();
+    olItens.innerHTML="";
     for(let i=0; i<lista.length; i++)
     {
         adicionaElementoNaLista(lista[i]);
     }
 }
 
-
+function adicionaItemDaEntrada(){
+    const valor = pEntrada.textContent;
+    adicionaNaLista (valor);
+    pEntrada.textContent = "";
+    atualizarListaOrdenada();
+}
 function adicionaElementoNaLista(texto){
     const li = document.createElement("li");
     li.textContent = texto;
