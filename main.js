d8c6b0730ac9dc7eea9351a51c2d4efb2ec20e59
rhara <rhara.ianna@estudante.ufjf.br>
2023-11-21 22:14:37 -0300

    criei 1 item da lista pela main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const nEntrada = document.querySelector("#entrada");
+const btnAdicionar = document.querySelector("#adicionar");
+const btnLimpar = document.querySelector("#limpar");
+const olItens = document.querySelector("#itens");
+
+const li = document.createElement("li");
+li.textContent = "Primeiro";
+olItens.appendChild(li)
