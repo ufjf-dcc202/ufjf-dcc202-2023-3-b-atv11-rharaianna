3ccf6f0d810d7de5994fead51a840bc92f8e2c67
rhara <rhara.ianna@estudante.ufjf.br>
2023-11-21 22:32:12 -0300

    criei lista.js e funções requisitadas

---
+++ src/lista.js
@@ -0,0 +1,23 @@
+const lista  = [
+    "um",
+    "dois",
+    "tres"
+];
+
+function getLista(){
+    return lista;
+}
+
+function adicionaNaLista(){
+
+}
+
+function removeDaLista(){
+
+}
+
+function limpaLista(){
+
+}
+
+export {getLista, adicionaNaLista, removeDaLista,limpaLista};
