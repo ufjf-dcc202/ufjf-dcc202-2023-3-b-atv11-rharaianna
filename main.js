e279c7b33612e1f182ea3cf93488a06b5ce00a2a
Rhara Ianna <rharaianna@gmail.com>
2023-11-22 20:02:58 -0300

    correções githum

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-import {getLista, adicionaNaLista, removeDaLista,limpaLista} from "../src/lista.js"
+import {
+  getLista,
+  adicionaNaLista,
+  removeDaLista,
+  limpaLista,
+} from "./lista.js";
 
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
@@ -6,31 +11,30 @@ const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
 atualizarListaOrdenada();
-btnAdicionar.addEventListener('click',adicionaItemDaEntrada);
-btnLimpar.addEventListener('click',limparListaOrdenada);
+btnAdicionar.addEventListener("click", adicionaItemDaEntrada);
+btnLimpar.addEventListener("click", limparListaOrdenada);
 
-function limparListaOrdenada(){
-    limpaLista();
-    atualizarListaOrdenada();
+function limparListaOrdenada() {
+  limpaLista();
+  atualizarListaOrdenada();
 }
 
-function atualizarListaOrdenada(){
-    const lista = getLista();
-    olItens.innerHTML="";
-    for(let i=0; i<lista.length; i++)
-    {
-        adicionaElementoNaLista(lista[i]);
-    }
+function atualizarListaOrdenada() {
+  const lista = getLista();
+  olItens.innerHTML = "";
+  for (let i = 0; i < lista.length; i++) {
+    adicionaElementoNaLista(lista[i]);
+  }
 }
 
-function adicionaItemDaEntrada(){
-    const valor = pEntrada.textContent;
-    adicionaNaLista (valor);
-    pEntrada.textContent = "";
-    atualizarListaOrdenada();
+function adicionaItemDaEntrada() {
+  const valor = pEntrada.textContent;
+  adicionaNaLista(valor);
+  pEntrada.textContent = "";
+  atualizarListaOrdenada();
 }
-function adicionaElementoNaLista(texto){
-    const li = document.createElement("li");
-    li.textContent = texto;
-    olItens.appendChild(li);
+function adicionaElementoNaLista(texto) {
+  const li = document.createElement("li");
+  li.textContent = texto;
+  olItens.appendChild(li);
 }
