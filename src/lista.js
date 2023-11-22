53f31dac33a38e192d168ef74af8a77171ce4e84
rhara <rhara.ianna@estudante.ufjf.br>
2023-11-21 22:41:08 -0300

    elaborei as funções

--- src/lista.js
+++ src/lista.js
@@ -1,23 +1,23 @@
 const lista  = [
-    "um",
-    "dois",
-    "tres"
+    "Um",
+    "Dois",
+    "Três"
 ];
 
 function getLista(){
     return lista;
 }
 
-function adicionaNaLista(){
-
+function adicionaNaLista(item){
+    lista.push(item);
 }
 
-function removeDaLista(){
-
+function removeDaLista(posicao){
+    lista.splice(posicao,1);
 }
 
 function limpaLista(){
-
+    lista.splice(0);
 }
 
 export {getLista, adicionaNaLista, removeDaLista,limpaLista};
