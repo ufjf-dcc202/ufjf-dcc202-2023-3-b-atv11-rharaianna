e9613edf38d75f5ebbefe64cce4213c5fd406c4e
rhara <rhara.ianna@estudante.ufjf.br>
2023-11-21 22:19:35 -0300

    Cria 3 li's

--- main.js
+++ main.js
@@ -5,4 +5,12 @@ const olItens = document.querySelector("#itens");
 
 const li = document.createElement("li");
 li.textContent = "Primeiro";
-olItens.appendChild(li)
+olItens.appendChild(li);
+
+const li2 = document.createElement("li");
+li2.textContent = "Segundo";
+olItens.appendChild(li2);
+
+const li3 = document.createElement("li");
+li3.textContent = "Terceiro";
+olItens.appendChild(li3);
